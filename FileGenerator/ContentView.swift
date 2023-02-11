a00a3c760dc4b5cf285c38f9b508d6a9ce745833
alexmorral <alexmorral@gmail.com>
2023-02-11 19:20:04 +0100

    Initial Commit

---
+++ FileGenerator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FileGenerator
+//
+//  Created by Alex Morral on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FileGenerator/FileGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileGeneratorApp.swift
+//  FileGenerator
+//
+//  Created by Alex Morral on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FileGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
